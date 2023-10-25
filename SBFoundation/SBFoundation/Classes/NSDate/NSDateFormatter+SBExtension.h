9ea5685c5c9932077d8186876a1da26aa0a7aa81
ankang4 <ankang4@gome.com.cn>
2023-10-25 14:31:49 +0800

    补充头文件

--- SBFoundation/SBFoundation/Classes/NSDate/NSDateFormatter+SBExtension.h
+++ SBFoundation/SBFoundation/Classes/NSDate/NSDateFormatter+SBExtension.h
@@ -5,7 +5,7 @@
 //  Created by 安康 on 2019/9/7.
 //
 
-
+#import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
--- SBFoundation/SBFoundation/Classes/XCPrePermission/XCPrePermissions.m
+++ SBFoundation/SBFoundation/Classes/XCPrePermission/XCPrePermissions.m
@@ -6,6 +6,10 @@
 //  Copyright (c) 2020 xiehaiduo. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
+
+#import <UIKit/UIKit.h>
+
 typedef NS_ENUM(NSInteger, XCTitleType) {
     XCTitleTypeRequest,
     XCTitleTypeDeny
